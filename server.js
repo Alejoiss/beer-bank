6e15ccee166f99fa35e6738e6802843ae1de448e
Joisson <joisson.mello@softfocus.com.br>
2019-03-05 08:54:46 -0300

    Heroku deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend-project'));
+app.use(express.static('./dist/beer-bank'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/frontend-project/index.html'));
+res.sendFile(path.join(__dirname,'/dist/beer-bank/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
